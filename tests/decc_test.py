95b929b54fd22bbd95b7fcd00d78d84498d5d9fc
david reid <zathrasorama@gmail.com>
2016-08-12 17:02:25 +0100

    Add a requirements.txt file for TravisCI integration. Update the decc test following recent updates.

--- tests/decc_test.py
+++ tests/decc_test.py
@@ -3,7 +3,9 @@ from pywind.decc.Report import *
 
 
 class DeccTest(TestCase):
+    """ DECC access tests. """
     def test_01(self):
+        """ Monthly Extract test """
         me = MonthlyExtract()
         self.assertTrue(me.get_data())
-        self.assertGreater(len(me), 7400)
+        self.assertGreater(len(me), 4000)
