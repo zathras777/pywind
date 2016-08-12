9c889b8caefb583118d2aba4f06e118e87fcecb6
david reid <zathrasorama@gmail.com>
2016-08-12 21:44:02 +0100

    Update the roc module to work better and allow access to more information. Also update test.

--- pywind/roc/eroc.py
+++ pywind/roc/eroc.py
@@ -1,49 +1,83 @@
+"""
+.. module:: pywind.roc
+"""
 from datetime import datetime
-from lxml import html
+import html5lib
 
-from pywind.ofgem.utils import get_url
+from pywind.utils import get_or_post_a_url
+
+
+def price_to_float(txt):
+    txt = txt.strip().replace(' ', '')
+    return float(txt[1:])
+
+
+def volume_to_int(txt):
+    if txt is None:
+        return 0
+    return int(txt.replace(',', ''))
 
 
 class EROCPrices(object):
+    """
+    Class that allows access to eROC ROC auction prices.
+    """
     URL = 'http://www.epowerauctions.co.uk/erocrecord.htm'
 
     def __init__(self):
+        self.auctions = []
         self.periods = {}
-        self.get_prices()
+
+    def process_file(self, local_file):
+        """
+        Process a local HTML file.
+        """
+        self.auctions = []
+        self.periods = {}
+        with open(local_file, "r") as lfh:
+            self._parse_content(lfh.read())
+        return True
 
     def get_prices(self):
-        try:
-            resp = get_url(self.URL)
-        except:
-            return False
+        """
+        Get the recent prices.
+        """
+        resp = get_or_post_a_url(self.URL)
+        self._parse_content(resp.content)
+        return True
 
-        def alltext(el):
-            return (''.join(el.text_content())).strip()
-
-        def process_table(tbl):
-            for row in tbl.findall('.//tr'):
-                if len(row) == 0:
+    def _parse_content(self, content):
+        document = html5lib.parse(content,
+                                  treebuilder="lxml",
+                                  namespaceHTMLElements=False)
+        for tbl in document.xpath('.//table'):
+            ths = tbl.xpath('.//tr//th')
+            if len(ths) == 0 or ths[0].text != 'Auction Date':
+                continue
+            for row in tbl.xpath('.//tr')[1:]:
+                row_data = row.xpath('td')
+                if len(row_data) == 0 or row_data[0].text.strip() == '':
                     continue
-                datestr = alltext(row[0])
-                if 'Auction Date' in datestr:
-                    continue
-
-                try:
-                    avg = float(alltext(row[1])[1:])
-                    dt = datetime.strptime(datestr, "%d %B %Y")
-                except ValueError:
-                    continue
-
-                numperiod = dt.year * 100 + dt.month
-                if numperiod in self.periods:
-                    self.periods[numperiod] = (self.periods[numperiod] + avg) / 2
+                dtt = datetime.strptime(row_data[0].text.strip(), "%d %B %Y").date()
+                auction_info = {'date': dtt,
+                                'average_price': price_to_float(row_data[1].text),
+                                'lowest_price': price_to_float(row_data[2].text),
+                                'total_volume': volume_to_int(row_data[3].text),
+                                'co_fired_volume': volume_to_int(row_data[4].text),
+                                'period': int("{}{:02d}".format(dtt.year, dtt.month))}
+                self.auctions.append(auction_info)
+        for info in self.auctions:
+            if info['period'] in self.periods:
+                previous = self.periods[info['period']]
+                if not isinstance(previous, list):
+                    self.periods[info['period']] = [info['average_price'], previous]
                 else:
-                    self.periods[numperiod] = avg
-
-        doc = html.parse(resp)
-        for t in doc.findall('.//table'):
-            if 'Auction Date' in alltext(t.findall('.//tr')[0]):
-                process_table(t)
+                    self.periods[info['period']].append(info['average_price'])
+            else:
+                self.periods[info['period']] = info['average_price']
+        for key in self.periods.keys():
+            if isinstance(self.periods[key], list):
+                self.periods[key] = sum(self.periods[key]) / len(self.periods[key])
         return True
 
     def __getitem__(self, item):
@@ -52,7 +86,10 @@ class EROCPrices(object):
         raise KeyError("No such period available [%s]" % item)
 
     def as_table_string(self):
-        s = ''
-        for p in sorted(self.periods.keys()):
-            s += "  {}       {:.02f}\n".format(p, self.periods[p])
-        return s
+        """
+        Function to return a nicely formatted period and average price.
+        """
+        sss = ''
+        for prd in sorted(self.periods.keys()):
+            sss += "  {}       {:.02f}\n".format(prd, self.periods[prd])
+        return sss
--- tests/roc_test.py
+++ tests/roc_test.py
@@ -1,11 +1,31 @@
+"""
+Tests for the pywind.roc module.
+"""
+import os
 import unittest
 
 from pywind.roc.eroc import *
 
 
 class ROCTest(unittest.TestCase):
+    """
+    ROC Tests
+    """
+    def test_local(self):
+        """
+        Local file test.
+        """
+        erp = EROCPrices()
+        here = os.path.dirname(__file__)
+        self.assertTrue(erp.process_file(os.path.join(here, 'eroc.html')))
+        self.assertTrue(200704 in erp.periods)
+        self.assertEqual(erp[200704], 47.51)
+
     def test_01(self):
-        ep = EROCPrices()
-        self.assertTrue(ep.get_prices())
-        self.assertTrue(200704 in ep.periods)
-        self.assertEqual(ep[200704], 47.51)
+        """
+        Remote test.
+        """
+        erp = EROCPrices()
+        self.assertTrue(erp.get_prices())
+        self.assertTrue(200704 in erp.periods)
+        self.assertEqual(erp[200704], 47.51)
