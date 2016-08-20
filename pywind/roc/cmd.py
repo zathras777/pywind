f2b3782d442f936c45cfe947b38fbe772ce4901b
david reid <zathrasorama@gmail.com>
2016-08-20 22:02:55 +0100

    Add the cmd.py file and update the class to work fully with new export mechanism.

---
+++ pywind/roc/cmd.py
@@ -0,0 +1,20 @@
+import sys
+
+from pywind.roc import EROCPrices
+
+
+def roc_prices(args):
+    print("eROC Auction Prices\n")
+
+    roc = EROCPrices()
+    if not roc.get_prices():
+        print("Unable to get prices from eROC website.")
+        sys.exit(0)
+
+    print("  Period         Average Price ")
+    print("  ------------   -------------")
+    for price in roc.prices():
+        print("  {:<12d}  {:>11.2f}".format(price[0], price[1]))
+
+    return roc
+
--- pywind/roc/eroc.py
+++ pywind/roc/eroc.py
@@ -3,24 +3,13 @@
 from datetime import datetime
 import html5lib
 
-from pywind.utils import get_or_post_a_url
-
-
-def price_to_float(txt):
-    txt = txt.strip().replace(' ', '')
-    return float(txt[1:])
-
-
-def volume_to_int(txt):
-    if txt is None:
-        return 0
-    return int(txt.replace(',', ''))
+from pywind.utils import get_or_post_a_url, _convert_type
 
 
 class EROCPrices(object):
     """
-    The EROCProces class provides access to the auction data from the eRIC website. \
-    Once created the :func:`get_prices()` is called to get the latest infrmation from their \
+    The EROCPrices class provides access to the auction data from the eRIC website.
+    Once created the :func:`get_prices()` is called to get the latest information from their
     website and parse the results into period prices.
 
     Once parsed period information can be accessed by using the class object as a dict, i.e. \
@@ -33,7 +22,7 @@ class EROCPrices(object):
     >>> eroc.get_prices()
     True
     >>> eroc[200701]
-
+    46.17
     """
     URL = 'http://www.epowerauctions.co.uk/erocrecord.htm'
 
@@ -49,7 +38,7 @@ class EROCPrices(object):
 
         :param local_file: Filename to be parsed.
         :returns: True or False
-        :rtype: boolean
+        :rtype: bool
         """
         self.auctions = []
         self.periods = {}
@@ -62,11 +51,29 @@ class EROCPrices(object):
         Get the recent prices.
 
         :return: True or False
-        :rtype: boolean
+        :rtype: bool
         """
         resp = get_or_post_a_url(self.URL)
         self._parse_content(resp.content)
         return True
+
+    def rows(self):
+        """ Generator to return the full auction data as rows.
+
+        :returns: Dict of price data
+        :rtype: dict
+        """
+        for auction in sorted(self.auctions, key=lambda x: x['date']):
+            yield {'Auction': {"@{}".format(key): auction[key] for key in auction}}
+
+    def prices(self):
+        """ Generator to return the calculated price data.
+
+        :return: Tuple of period, price
+        :rtype: tuple
+        """
+        for prc in sorted(self.periods.keys()):
+            yield (prc, self.periods[prc])
 
     def _parse_content(self, content):
         document = html5lib.parse(content,
@@ -82,10 +89,10 @@ class EROCPrices(object):
                     continue
                 dtt = datetime.strptime(row_data[0].text.strip(), "%d %B %Y").date()
                 auction_info = {'date': dtt,
-                                'average_price': price_to_float(row_data[1].text),
-                                'lowest_price': price_to_float(row_data[2].text),
-                                'total_volume': volume_to_int(row_data[3].text),
-                                'co_fired_volume': volume_to_int(row_data[4].text),
+                                'average_price': _convert_type(row_data[1].text.strip()[1:], 'float'),
+                                'lowest_price': _convert_type(row_data[2].text.strip()[1:], 'float'),
+                                'total_volume': _convert_type(row_data[3].text or '0', 'int'),
+                                'co_fired_volume': _convert_type(row_data[4].text or '0', 'int'),
                                 'period': int("{}{:02d}".format(dtt.year, dtt.month))}
                 self.auctions.append(auction_info)
         for info in self.auctions:
@@ -106,14 +113,3 @@ class EROCPrices(object):
         if item in self.periods:
             return self.periods[item]
         raise KeyError("No such period available [%s]" % item)
-
-    def as_table_string(self):
-        """
-        Function to return a nicely formatted period and average price.
-
-        :returns: String representation of data as a table.
-        """
-        sss = ''
-        for prd in sorted(self.periods.keys()):
-            sss += "  {}       {:.02f}\n".format(prd, self.periods[prd])
-        return sss
