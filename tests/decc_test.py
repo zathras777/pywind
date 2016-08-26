b238fa34a0a6d4685aa720127efcec407a0d65b8
david reid <zathrasorama@gmail.com>
2016-08-26 18:07:43 +0100

    Catch up with change of module for decc

--- tests/decc_test.py
+++ tests/decc_test.py
@@ -1,11 +1,13 @@
+import os
 from unittest import TestCase
-from pywind.decc.Report import *
+from pywind.decc.extract import *
 
 
 class DeccTest(TestCase):
     """ DECC access tests. """
+    HERE = os.path.dirname(__file__)
     def test_01(self):
         """ Monthly Extract test """
-        me = MonthlyExtract()
-        self.assertTrue(me.get_data())
-        self.assertGreater(len(me), 4000)
+        dme = MonthlyExtract(filename=os.path.join(self.HERE, 'files', 'decc_extract.csv'))
+        self.assertTrue(dme.get_data())
+        self.assertEqual(len(dme), 4896)
